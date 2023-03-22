1a9df6f904fd2170e9f17848600fe7db99c94611
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-03-22 10:44:05 -0600

    math complete

--- js/circle.js
+++ js/circle.js
@@ -9,16 +9,18 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return (Math.pow(this.radius, 2) * Math.PI);// TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding === true) {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea(this.radius)));
+            } else {
+                // If doRounding is true, round the result to the nearest integer.
+                // Otherwise, output the complete value
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea(this.radius));
+            }
         }
     };
 
@@ -30,6 +32,11 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+// function changeRadius(x){
+//     circle.radius = x;
+// }
+// changeRadius(5);
+    circle['radius'] = 5;
 
     // log info about the circle
     console.log("Raw circle information");
