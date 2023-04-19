1f5bdbe9bf059b0c5a777f39aa65d1a1acf98ce2
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-04-19 14:21:14 -0600

    mouse events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -8,20 +8,47 @@
     // var contents = $('#title').html();
     // alert(contents);
 
-    $('#title, p').css('color', 'blue');
+    // $('#title, p').css('color', 'blue');
+    //
+    // $('.codeup, h1, p').css('background-color', 'yellow');
+    //
+    // $('li').css('font-size', '20px');
+    //
+    // $('body').css('background-color', 'gray');
+    //
+    // $('ul').css('max-width', '100px');
+    //
+    // $('*').css('text-align', 'center');
+    //
+    // $('.divs').css('border', '5px solid #F00');
+    //
+    // $('#codeup').css('color', 'red');
 
-    $('.codeup, h1, p').css('background-color', 'yellow');
 
-    $('li').css('font-size', '20px');
+    $('h1').click(
+        function () {
+            $(this).css('background-color', 'yellow');
+            $(this).css('text-align', 'center');
+            $(this).css('font-size', '50px');
+        }
+    );
 
-    $('body').css('background-color', 'gray');
+    $('p').dblclick(
+        function () {
+            $(this).css('font-size', '18px');
+        }
+    );
 
-    $('ul').css('max-width', '100px');
+    $('li').hover(
+        function () {
+            $(this).css('color', 'red');
+        },
+        function () {
+            $(this).css('color', 'black')
+        }
+    );
 
-    $('*').css('text-align', 'center');
 
-    $('.divs').css('border', '5px solid #F00');
 
-    $('#codeup').css('color', 'red');
 
 })();
