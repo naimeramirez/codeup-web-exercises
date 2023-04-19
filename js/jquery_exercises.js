b6eb48e6bc9a0ddacaa78b756bf15ca2dd53ddd4
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-04-19 09:52:18 -0600

    jquery

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,9 +1,27 @@
 (function () {
     "use strict";
 
-    $(function() {
-        alert( 'The DOM has finished loading!' );
-    });
+    // $(function() {
+    //     alert( 'The DOM has finished loading!' );
+    // });
 
+    // var contents = $('#title').html();
+    // alert(contents);
+
+    $('#title, p').css('color', 'blue');
+
+    $('.codeup, h1, p').css('background-color', 'yellow');
+
+    $('li').css('font-size', '20px');
+
+    $('body').css('background-color', 'gray');
+
+    $('ul').css('max-width', '100px');
+
+    $('*').css('text-align', 'center');
+
+    $('.divs').css('border', '5px solid #F00');
+
+    $('#codeup').css('color', 'red');
 
 })();
