9a34ac9475338e4a4389f1ef6aaa1a5656c4454d
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-04-27 23:37:20 -0600

    changed temp

--- js/weather_map.js
+++ js/weather_map.js
@@ -10,55 +10,52 @@
     }).done(function (weather) {
         // console.log(weather);
         $('#currentCity').html(`${weather.name}`);
-        $('#currentTemp').html(`Current Temperature: ${weather.main.temp}°F`);
+        $('#currentTemp').html(`Current Temperature: ${weather.main.temp.toString().slice(0, 2)}°F`);
         $('#currentWeatherIcon').html(`<img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png">`);
-        $('#currentWeather').html(`Description: ${weather.weather[0].description}`);
+        $('#currentWeather').html(`${weather.weather[0].description}`);
         $('#currentHumidity').html(`Humidity: ${weather.main.humidity}%`);
-        $('#currentWind').html(`Wind Speed: ${weather.wind.speed} mph`);
+        $('#currentWind').html(`Wind: ${weather.wind.speed} mph`);
         $('#currentPressure').html(`Pressure: ${weather.main.pressure} hPa`);
     });
 
 //5 day forecast
+    function forecastDay(day) {
+        var html = `<div class="col">`;
+        html += `<div class="card">`;
+        html += `<div class="card-header">${day.dt_txt.slice(0, 10)}</div>`;
+        html += `<div class="card-body">`;
+        html += `<p class="card-title fw-bold">${day.main.temp_min.toString().slice(0, 2)}°F / ${day.main.temp_max.toString().slice(0, 2)}°F</p>`;
+        html += `<img src="http://openweathermap.org/img/w/${day.weather[0].icon}.png">`;
+        html += `<p class="card-text fw-bold">${day.weather[0].description}</p>`;
+        html += `<p class="card-text">Humidity: ${day.main.humidity}%</p>`;
+        html += `<p class="card-text">Wind: ${day.wind.speed} mph</p>`;
+        html += `<p class="card-text">Pressure: ${day.main.pressure} hPa</p>`;
+        html += `</div>`;
+        html += `</div>`;
+        html += `</div>`;
+        return html;
+    }
+
+    function forecastDays(days) {
+        var html = '';
+        for (var i = 0; i < days.length; i += 8) {
+            // console.log(days[i]);
+            html += forecastDay(days[i]);
+        }
+        return html;
+    }
+
     $.get("http://api.openweathermap.org/data/2.5/forecast", {
         APPID: OPEN_WEATHER_APPID,
         q: "El Paso, US",
         units: "imperial"
     }).done(function (weather) {
         // console.log(weather);
-
-        function forecastDay(day) {
-            var html = `<div class="col">`;
-            html += `<div class="card">`;
-            html += `<div class="card-header">${day.dt_txt.slice(0,9)}</div>`;
-            html += `<div class="card-body">`;
-            html += `<p class="card-title">${day.main.temp_min}°F / ${day.main.temp_max}°F</p>`;
-            html += `<img src="http://openweathermap.org/img/w/${day.weather[0].icon}.png">`;
-            html += `<p class="card-text">${day.weather[0].description}</p>`;
-            html += `<p class="card-text">Humidity: ${day.main.humidity}%</p>`;
-            html += `<p class="card-text">Wind: ${day.wind.speed} mph</p>`;
-            html += `<p class="card-text">Pressure: ${day.main.pressure} hPa</p>`;
-            html += `</div>`;
-            html += `</div>`;
-            html += `</div>`;
-            return html;
-        }
-
-        function forecastDays(days) {
-            var html = '';
-            for (var i = 0; i < days.length; i += 8) {
-                // console.log(days[i]);
-                html += forecastDay(days[i]);
-            }
-            return html;
-        }
-
+        // console.log(weather.list);
         $('#forecastCards').html(forecastDays(weather.list));
+    });
 
-        console.log(weather.list);
-
-    })
-
-
+// mapbox
     mapboxgl.accessToken = MAPBOX_KEY;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
