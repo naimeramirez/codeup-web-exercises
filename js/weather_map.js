739610e21f1950f0caaea5482356eaf21bd0b4c7
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-04-27 22:49:59 -0600

    cards added

--- js/weather_map.js
+++ js/weather_map.js
@@ -8,10 +8,15 @@
         q: "El Paso, US",
         units: "imperial"
     }).done(function (weather) {
-        console.log(weather);
-
-        $('#currentTemp').html(`<p>${weather.main.temp}</p>`)
-    })
+        // console.log(weather);
+        $('#currentCity').html(`${weather.name}`);
+        $('#currentTemp').html(`Current Temperature: ${weather.main.temp}°F`);
+        $('#currentWeatherIcon').html(`<img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png">`);
+        $('#currentWeather').html(`Description: ${weather.weather[0].description}`);
+        $('#currentHumidity').html(`Humidity: ${weather.main.humidity}%`);
+        $('#currentWind').html(`Wind Speed: ${weather.wind.speed} mph`);
+        $('#currentPressure').html(`Pressure: ${weather.main.pressure} hPa`);
+    });
 
 //5 day forecast
     $.get("http://api.openweathermap.org/data/2.5/forecast", {
@@ -19,9 +24,38 @@
         q: "El Paso, US",
         units: "imperial"
     }).done(function (weather) {
-        console.log(weather);
+        // console.log(weather);
 
-        $('#forecastTemp').html(`${weather.list[0].main.temp}`)
+        function forecastDay(day) {
+            var html = `<div class="col">`;
+            html += `<div class="card">`;
+            html += `<div class="card-header">${day.dt_txt.slice(0,9)}</div>`;
+            html += `<div class="card-body">`;
+            html += `<p class="card-title">${day.main.temp_min}°F / ${day.main.temp_max}°F</p>`;
+            html += `<img src="http://openweathermap.org/img/w/${day.weather[0].icon}.png">`;
+            html += `<p class="card-text">${day.weather[0].description}</p>`;
+            html += `<p class="card-text">Humidity: ${day.main.humidity}%</p>`;
+            html += `<p class="card-text">Wind: ${day.wind.speed} mph</p>`;
+            html += `<p class="card-text">Pressure: ${day.main.pressure} hPa</p>`;
+            html += `</div>`;
+            html += `</div>`;
+            html += `</div>`;
+            return html;
+        }
+
+        function forecastDays(days) {
+            var html = '';
+            for (var i = 0; i < days.length; i += 8) {
+                // console.log(days[i]);
+                html += forecastDay(days[i]);
+            }
+            return html;
+        }
+
+        $('#forecastCards').html(forecastDays(weather.list));
+
+        console.log(weather.list);
+
     })
 
 
