c0e32ada1d39eaf05aa3ad7c8cb4156be4e23fc2
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-05-05 16:02:38 -0600

    map filter reduce

--- js/es6.js
+++ js/es6.js
@@ -1,6 +1,7 @@
 (function (){
     "use strict";
 
+
     /*
      * Complete the TODO items below
      */
@@ -32,56 +33,70 @@
         }
     ];
 
+    console.log(users);
+
 // TODO: fill in your name and email and add some programming languages you know
 // to the languages array
+    users.push({name: 'naime', email: 'naimeramirez@gmail.com', languages: ['html', 'css', 'javascript']});
+
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-    var name = 'your_name_here';
-    var email = '';
-    var languages = [];
+    const name = 'naime';
+    const email = 'naimeramirez@gmail.com';
+    const languages = ['html', 'css', 'javascript'];
 
 // TODO: rewrite the object literal using object property shorthand
     users.push({
-        name: name,
-        email: email,
-        languages: languages
+        name,
+        email,
+        languages
     });
-
+    console.log(users);
 // TODO: replace `var` with `let` in the following variable declarations
-    var emails = [];
-    var names = [];
+    let emails = [];
+    let names = [];
 
 // TODO: rewrite the following using arrow functions
-    users.forEach(function(user) {
+    users.forEach((user) => {
         return emails.push(user.email);
     });
-    users.forEach(function(user) {
+    users.forEach((user) => {
         return names.push(user.name);
     });
 
+    users.forEach(user => emails.push(user.email));
+    users.forEach(user => names.push(user.name));
+
 // TODO: replace `var` with `let` in the following declaration
-    var developers = [];
-    users.forEach(function(user) {
+    let developers = [];
+
+    // users.forEach(({name, email}) => {
+
+    users.forEach(function({user}) {
+
         // TODO: rewrite the code below to use object destructuring assignment
         //       note that you can also use destructuring assignment in the function
         //       parameter definition
-        const name = user.name;
-        const email = user.email;
-        const languages = user.languages;
+        // const name = user.name;
+        // const email = user.email;
+        // const languages = user.languages;
+        const {name, email, languages} = user;
+
+        console.log(name, email, languages)
 
         // TODO: rewrite the assignment below to use template strings
-        developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+        developers.push(`${name}'s email is ${email} ${name} knows  ${languages.join(', ')}`);
     });
 
 // TODO: Use `let` for the following variable
-    var list = '<ul>';
+    let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-    developers.forEach(function (developer) {
+    for (let developer of developers) {
 
         // TODO: rewrite the assignment below to use template strings
-        list += '<li>' + developer + '</li>';
-    });
+        list += `<li> ${developer} </li>`;
+    };
     list += '</ul>';
 
 
---
+++ js/map-filter-reduce.js
@@ -0,0 +1,108 @@
+(function (){
+    "use strict";
+
+    const users = [
+        {
+            id: 1,
+            name: 'ryan',
+            email: 'ryan@codeup.com',
+            languages: ['clojure', 'javascript'],
+            yearsOfExperience: 5
+        },
+        {
+            id: 2,
+            name: 'luis',
+            email: 'luis@codeup.com',
+            languages: ['java', 'scala', 'php'],
+            yearsOfExperience: 6
+        },
+        {
+            id: 3,
+            name: 'zach',
+            email: 'zach@codeup.com',
+            languages: ['javascript', 'bash'],
+            yearsOfExperience: 7
+        },
+        {
+            id: 4,
+            name: 'fernando',
+            email: 'fernando@codeup.com',
+            languages: ['java', 'php', 'sql'],
+            yearsOfExperience: 8
+        },
+        {
+            id: 5,
+            name: 'justin',
+            email: 'justin@codeup.com',
+            languages: ['html', 'css', 'javascript', 'php'],
+            yearsOfExperience: 9
+        }
+    ];
+
+//    Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+    const languagesX3 = users.filter(users => users.languages.length >= 3);
+    console.log(languagesX3);
+
+    // var threeLanguages = users.filter(function (person) {
+    //     return person.languages.length > 2;
+    // })
+
+//    Use .map to create an array of strings where each element is a user's email address
+    const emailAddress = users.map(users => users.email);
+    console.log(emailAddress);
+
+    // var useEmail = users.map(function (person) {
+    //     return person.email;
+    // });
+
+//    Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+    const years = users.reduce((total, person) => {
+        return (total + person.yearsOfExperience);
+    }, 0);
+    console.log(years);
+
+    // var totalExp = users.reduce(function(total, person){
+    //     return total + person.yearsOfExperience;
+    // }, 0);
+
+    const averageYears = years / users.length;
+    console.log(averageYears);
+
+//    Use .reduce to get the longest email from the list of users.
+//     const longestEmail = emailAddress.reduce((longest, current) => {
+//         if (current.length > longest) {
+//             longest = current;
+//         }
+//     }, 0);
+
+
+    var longestEmail = users.reduce(function (longEmail, person){
+        if (person.email.length > longEmail.length) {
+            longEmail = person.email;
+        }
+        return longEmail;
+    }, "");
+        console.log(longestEmail);
+
+//    Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+//     const instructors = users.reduce((names, person) => {
+//         return person.name;
+//     }, "");
+//     console.log("Your instructors are: " + instructors);
+
+    const instructors = users.reduce(function (names, person) {
+        if (names === "") {
+            names = "Your instructors are: " + person.name;
+        } else if (names !== "") {
+            names += ", " + person.name;
+        }
+        return names;
+    }, "");
+    console.log(instructors);
+
+    // const nameString = users.reduce((nString, userName) => (email.length > userEmail.length) ? email : userEmail.email, )
+
+//    Use .reduce to get the unique list of languages from the list of users.
+
+
+})();
