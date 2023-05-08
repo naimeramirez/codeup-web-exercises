1d0f940a58e00449429d34806d11d53209b8e124
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-05-08 13:28:54 -0600

    notes

--- js/promises.js
+++ js/promises.js
@@ -3,23 +3,27 @@
 
     const userName = 'naimeramirez';
 
-    let commits = [];
-
     function getData(user) {
         return fetch(`https://api.github.com/users/${user}/events`, {headers: {'Authorization': GITHUB_API_TOKEN}})
             .then(response => response.json())
             .then(data => {
-                data.forEach(pushEvent => {
-                    console.log(pushEvent);
-                    if (pushEvent.type === 'PushEvent'){
-                        commits.push(pushEvent.created_at);
-                    }
-                })
+                console.log(data[0].created_at);
             });
     };
 
     getData(userName);
-    console.log(commits);
+
+    function wait(x) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                    resolve('Here is your data: ...');
+            }, x);
+        });
+    }
+
+    wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+    wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
 
 
 })();
